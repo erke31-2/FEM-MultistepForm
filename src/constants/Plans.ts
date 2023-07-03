38f4674d337a22fad159c6ab87d52fe11b5df795
erke31-2 <KaungSet>
2023-07-03 23:42:54 +0630

    updated

--- src/constants/Plans.ts
+++ src/constants/Plans.ts
@@ -1,39 +1,41 @@
-export interface Plan{
-    title: string;
-    img: string;
-    
-    billing: {
-        monthly: number;
-        yearly: number
-    }
+import arcadeIcon from "../assets//images/icon-arcade.svg"
+import advancedIcon from "../assets/images/icon-advanced.svg"
+import proIcon from "../assets/images/icon-pro.svg"
+export interface Plan {
+  title: string;
+  img: string;
+
+  billing: {
+    monthly: number;
+    yearly: number;
+  };
 }
 
-const  plans:Plan[] = [
-    {
-        title: "Arcade",
-        img: "src/assets/images/icon-arcade.svg",
-        billing: {
-            monthly: 9,
-            yearly: 90
-        }
+const plans: Plan[] = [
+  {
+    title: "Arcade",
+    img: arcadeIcon,
+    billing: {
+      monthly: 9,
+      yearly: 90,
     },
-    {
-        title: "Advanced",
-        img: "src/assets/images/icon-advanced.svg",
-        billing: {
-            monthly: 12,
-            yearly: 120
-        }
+  },
+  {
+    title: "Advanced",
+    img: advancedIcon,
+    billing: {
+      monthly: 12,
+      yearly: 120,
     },
-    {
-        title: "Pro",
-        img: "src/assets/images/icon-pro.svg",
-        billing: {
-            monthly: 15,
-            yearly: 150
-        }
+  },
+  {
+    title: "Pro",
+    img: proIcon,
+    billing: {
+      monthly: 15,
+      yearly: 150,
     },
-]
+  },
+];
 
-
-export default plans
+export default plans;
